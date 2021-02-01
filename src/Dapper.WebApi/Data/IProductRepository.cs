3daa191122688ca0c9abb6e03822fd4280cbbd88
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-02-01 14:11:26 +0700

    Add Generic Repository Dapper.

---
+++ src/Dapper.WebApi/Data/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using Dapper.WebApi.Data.Entities;
+
+namespace Dapper.WebApi.Data
+{
+    public interface IProductRepository : IGenericRepository<Product>
+    {
+    }
+}
---
+++ src/Dapper.WebApi/Data/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace Dapper.WebApi.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IProductRepository productRepository)
+        {
+            Products = productRepository;
+        }
+        public IProductRepository Products { get; }
+    }
+}
